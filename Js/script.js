5bac3a9ea2eb4d6a2f9ee0ec45d8959beb32ecef
jujuck <julien.y.richard@gmail.com>
2018-03-16 11:16:47 +0100

    mise en place des animations de pays details>carte

--- Js/script.js
+++ Js/script.js
@@ -1,18 +1,19 @@
-//Création du menu central
-function initMenuPays () {
+//==========Création générale de l'espace Pays===========
+//===============Création du menu central=============
+function initPaysCarte () {
     var myRequestMenu = new XMLHttpRequest();
     myRequestMenu.open('GET','Data/container-bloc.json');
 
     myRequestMenu.onreadystatechange = function () {
         if(myRequestMenu.readyState === 4) {
             var menu = JSON.parse(myRequestMenu.responseText);
-            var menuList = '<div class="row row-pays">';
+            var menuList = '<div class="row row-pays-carte">';
 
             for (var i = 0; i < menu.length; i++) {
-                menuList += '<div class="col-xs-12 col-sm-6 col-md-3 nos-pays-bloc">';
-                menuList += '<div class="nos-pays-item">'
+                menuList += '<div class="col-xs-12 col-sm-6 col-md-3 nos-pays-carte">';
+                menuList += '<div class="nos-pays-carte-item wow zoomIn" data-wow-duration:"1s">'
                 menuList += '<h3>' + menu[i].name + '</h3>';
-                menuList += '<img class="image-pays" src="' + menu[i].image + '">';
+                menuList += '<img class="image-pays-carte" src="' + menu[i].image + '">';
                 menuList += '<button class="btn btn-pays">Let s visit!</button>';
                 menuList += '</div>';
                 menuList += '</div>';
@@ -25,8 +26,39 @@ function initMenuPays () {
     myRequestMenu.send();
 }
 
-initMenuPays();
+//initPaysCarte();
 
+//=============Création du menu central=====================
+function initPaysListe () {
+    var myRequestMenu = new XMLHttpRequest();
+    myRequestMenu.open('GET','Data/container-bloc.json');
+
+    myRequestMenu.onreadystatechange = function () {
+        if(myRequestMenu.readyState === 4) {
+            var menu = JSON.parse(myRequestMenu.responseText);
+            var menuList = '<div class="pays-détail">';
+
+            for (var i = 0; i < menu.length; i++) {
+                menuList += '<div class="row row-pays-detail wow rubberBand" data-wow-duration:"1s">'
+                menuList += '<div class="col-md-2 nos-pays-detail-titre">';
+                menuList += '<h3>' + menu[i].name + '</h3>';
+                menuList += '</div>';
+                menuList += '<div class="col-md-2 nos-pays-detail-img">'
+                menuList += '<img class="image-pays-detail" src="' + menu[i].image + '">';
+                menuList += '</div>';
+                menuList += '<div class="col-md-8 nos-pays-detail-text">';
+                menuList += '<p>' + menu[i].text + '<a href="#">Lire la suite...</a><p>'
+                menuList += '</div>';
+                menuList += '</div>'; 
+            }
+        }
+        menuList += '</div>';
+        document.getElementById('container-pays').innerHTML = menuList;
+    };
+    myRequestMenu.send();
+}
+
+initPaysListe();
 
 //Création du menu produit
 function initMenuProduit () {
@@ -40,7 +72,7 @@ function initMenuProduit () {
 
             for (var i = 0; i < menuProd.length; i++) {
                 produitList += '<div class="col-xs-12 col-sm-8 col-md-4 nos-produit-bloc">';
-                produitList += '<div class="nos-produits-item">';
+                produitList += '<div class="nos-produits-item wow jackInTheBox" data-wow-duration:"1s">';
                 produitList += '<h3>' + menuProd[i].name + '</h3>';
                 produitList += '<p class="nos-produit-text">' + menuProd[i].text + '</p>';
                 produitList += '<button class="btn btn-pays">Let s Go!</button>';
